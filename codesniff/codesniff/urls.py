624e99f5c4e38aef30d187b39ea642e75b79cb23
Hanna Koh <hannakoh@wirelessprvnat-172-17-244-3.near.illinois.edu>
2015-11-30 20:59:12 -0600

    Starting work on rest framework

--- codesniff/codesniff/urls.py
+++ codesniff/codesniff/urls.py
@@ -1,5 +1,22 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +25,6 @@ urlpatterns = patterns('',
 
     url(r'^app/', include('app.urls', namespace='app')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
