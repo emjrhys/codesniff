9442fbc513b4cc323ce9283be7e35b162549b1c0
Hanna Koh <hannakoh@wirelessprvnat-172-17-244-3.near.illinois.edu>
2015-11-30 21:47:14 -0600

    Added api calls for Users and Code

--- codesniff/app/admin.py
+++ codesniff/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from app.models import Code, Scores, CodeSmells
 # Register your models here.
+
+admin.site.register(Code)
+admin.site.register(Scores)
+admin.site.register(CodeSmells)
+
--- codesniff/codesniff/urls.py
+++ codesniff/codesniff/urls.py
@@ -1,23 +1,38 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from app.models import Code, Scores, CodeSmells
 from rest_framework import routers, serializers, viewsets
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'is_staff')
+        fields = ('username','email')
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+# Serializers define the API representation.
+class CodeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Code
+        fields = ('content','language','creator','date_added')
+
+# ViewSets define the view behavior.
+class CodeViewSet(viewsets.ModelViewSet):
+    queryset = Code.objects.all()
+    serializer_class = CodeSerializer
+
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'code', CodeViewSet)
 
+# Url patterns
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'codesniff.views.home', name='home'),
