bb9bae01c88000fe82a8e2a70d210bb4bb003278
Annie Rong <annie@Annies-MacBook-Pro-2.local>
2015-12-08 00:58:08 -0600

    Fixed dropdown

--- client/src/components/SubmitCode.js
+++ client/src/components/SubmitCode.js
@@ -7,7 +7,7 @@ class SubmitCode extends Component {
 		super(props);
 		this.state = {
 			value: '(e.g. CS 242 - Chess)',
-			defaultLanguage: {value: 'Java'}
+			language: {value: 'Java'}
 		};
 		// React components in ES6 no long autobinds this to nonReact methods
 		this.handleChange = this.handleChange.bind(this);
@@ -18,14 +18,14 @@ class SubmitCode extends Component {
 			value: evt.target.value
 		});
 	}
-	handleSelect(language) {
+	handleSelect(evt) {
 		this.setState({
-			defaultLanguage: language
+			language: evt.target.value
 		});
 	}
     
     handleSubmit() {
-        codeService.submitCode(this.state.value, this.state.defaultLanguage);
+        codeService.submitCode(this.state.value, this.state.language);
     }
 
 	render() {
@@ -41,7 +41,16 @@ class SubmitCode extends Component {
 				<p>Logged in as <span className="profile">John</span></p>
                 <form>
 					<label className="title">Title<input placeholder={this.state.value} onChange={this.handleChange}/></label>
-					<label><span>Language</span><input placeholder="Java"/></label>
+					<label><span>Language</span>
+						<select onChange={this.handleSelect}>
+							<option value="" disabled selected>select a language</option>
+							{
+								languages.map(function(language) {
+									return (<option id={language.id} value={language.value}>{language.value}</option>)
+								})
+							}
+						</select>
+					</label>
 					<textarea placeholder="paste or drag your code here">
 					</textarea>
 					<button
