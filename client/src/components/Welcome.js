34bd17ba3f726e34d9ffb6a5d8b8b5a6792d728c
Annie Rong <annie@wirelessprvnat-172-17-20-200.near.illinois.edu>
2016-01-28 16:29:31 -0600

    added routing from Welcome to SubmitCode

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,19 +1,27 @@
 import React, { PropTypes, Component } from 'react'
 
 class Welcome extends Component {
-
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.context.history.pushState(null, '/submit');
+    }
     render() {
         return (
             <div className="component-welcome">
                 <h1>CodeSniff</h1>
                 <h2>Crowdsource your refactoring</h2>
                 <form>
-	                <button type="button" className="cta">Log In</button>
+	                <button type="button" className="cta" onClick={this.handleClick}>Log In</button>
 	                <button type="button">Sign Up</button>
                 </form>
             </div>
         )
     }
 }
+
+Welcome.contextTypes = { history: React.PropTypes.object.isRequired }
 
 export default Welcome
