6cc13dc90ea5c1a46c6e6f7edeea8e7ffb9f4e67
Annie Rong <annie@wirelessprvnat-172-17-172-188.near.illinois.edu>
2016-03-11 16:39:10 -0600

    hello andy

--- client/src/reducers/code.js
+++ client/src/reducers/code.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import { SEND_CODE, REQUEST_CODE, RECEIVE_CODE } from '../constants/ActionTypes';
-
+import { SEND_CODE, REQUEST_CODE, RECEIVE_CODE, SELECT_CODE } from '../constants/ActionTypes';
+import _ from 'underscore';
 
 export default function codes(state = {}, action) {
     switch (action.type) {
@@ -17,7 +17,16 @@ export default function codes(state = {}, action) {
                 isFetching: false,
                 codeReview: action.code
             });
+
+        case SELECT_CODE:
+            let selectedLines = state.selectedLines || {};
+            selectedLines[action.payload.lineNumber] = action.payload.codeSmellId; 
+
+            return Object.assign({}, state, {
+                selectedLines
+            });
+
         default:
-            return {}
+            return state;
     }
 }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,11 +3,13 @@ import { reduxReactRouter, routerStateReducer, ReduxRouter } from 'redux-router'
 import code from './code'
 import auth from './auth'
 import user from './user'
+import smells from './smells'
 
 const rootReducer = combineReducers({
     code,
     auth,
     user,
+    smells,
     router: routerStateReducer,
 });
 
