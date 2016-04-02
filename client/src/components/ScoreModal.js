f2b73f493f04902f2885585e76b40196357abaa5
Annie Rong <annie@Annies-MacBook-Pro-2.local>
2016-04-02 16:50:26 -0500

    added score modal after review code

---
+++ client/src/components/ScoreModal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { connect } from 'react-redux';
+
+class ScoreModal extends React.Component {
+
+	render() {
+		if (this.props.isOpen) {
+			return (
+				<ReactCSSTransitionGroup
+					transitionName={this.props.transitionName}
+					transitionEnterTimeout={this.props.transitionEnterTimeout}
+                    transitionLeaveTimeout={this.props.transitionLeaveTimeout}>
+					<div className={this.props.className}>
+						{this.props.children}
+					</div>
+				</ReactCSSTransitionGroup>
+				);
+		} else {
+			return <ReactCSSTransitionGroup 
+					transitionName={this.props.transitionName}
+					transitionEnterTimeout={this.props.transitionEnterTimeout}
+                    transitionLeaveTimeout={this.props.transitionLeaveTimeout} />;
+		}
+	}
+}
+
+export default ScoreModal;
