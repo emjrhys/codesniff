999aeaf68ee104174d2dc20840adb72a89e5fb0b
Annie Rong <annie@Annies-MacBook-Pro-2.local>
2016-04-25 09:04:47 -0500

    actions documentation

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -5,13 +5,19 @@ import {
 } from '../constants/ActionTypes';
 import request from 'superagent';
 
-
+/**
+ * Begins the user request.
+ */
 export function getUserRequest() {
     return {
         type: REQUEST_USER_INFO,
     }
 }
 
+/**
+ * Get user success assigns the retrieved data to the payload.
+ * @param data
+ */
 export function getUserSuccess(data) {
     return {
         type: GET_USER_SUCCESS,
@@ -19,12 +25,21 @@ export function getUserSuccess(data) {
     }
 }
 
+/** 
+ * Get user failure marks the attempt as a failure.
+ * @param err
+ *
+ * TODO: Don't think we need err, but maybe I'm wrong.
+ */
 export function getUserFailure(err) {
     return {
         type: GET_USER_FAILURE,
     }
 }
 
+/**
+ * Get user info attempts to retrieve information about the user. 
+ */
 export function getUserInfo() {
     return function (dispatch) {
         
@@ -36,8 +51,7 @@ export function getUserInfo() {
             .end(function(err, res) {
                 if(err || !res.ok) {
                     dispatch(getUserFailure(err));
-                }
-                else {
+                } else {
                     dispatch(getUserSuccess(JSON.parse(res.text)));
                 }
             });
