7930e738f8f0dadf4d5da22a38654e579b11d52a
Annie Rong <annie@Annies-MacBook-Pro-2.local>
2016-04-25 08:26:41 -0500

    removed unused reducer; added reducer doc

--- client/src/reducers/smells.js
+++ client/src/reducers/smells.js
@@ -5,6 +5,10 @@ import {
 	RECEIVE_SCORE
 } from '../constants/ActionTypes';
 
+/**
+ * Initial state for a smells state, which has a negative score and empty arrays for the correct,
+ * incorrect, and missed lines.
+ */
 const initialState = {
 	score: -1,
 	missed: [],
@@ -12,6 +16,15 @@ const initialState = {
 	incorrect: []
 }
 
+/**
+ * The smells reducer handles 4 different actions:
+ * 1. Clear code smells clears every change we've made and returns the initialState.
+ * 2. Receive code smells returns all the code smells we have in the backend, currently not being used.
+ * 3. Request code smells is called the beginning of fetchCodeSmells and it marks our intent to request.
+ *    NOTE: not sure why it's assigned codeSmells
+ * 4. Receive score is called at the end of a successful code smells and assigns the score, correct,
+ *    incorrect and missed lines that's to be displayed by the check code smells component.
+ */
 export default function codesmells(state = initialState, action) {
 	switch (action.type) {
 		case CLEAR_CODESMELLS:
